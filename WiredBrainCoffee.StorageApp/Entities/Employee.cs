69df9b37ec6dc5390e5c401d2d06c8eaf122c38d
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-26 14:48:21 +1200

    create Employee class

---
+++ WiredBrainCoffee.StorageApp/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace WiredBrainCoffee.StorageApp.Entities
+{
+    public class Employee
+    {
+        //properties
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+
+        //methods
+        public override string ToString() => $"Id: {Id} FirstName: {FirstName}";
+    }
+}
