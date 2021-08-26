b9c9f6df8e30eaa8f17410a39b297039f9da63b7
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-26 13:25:36 +1200

    initial commit

---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
