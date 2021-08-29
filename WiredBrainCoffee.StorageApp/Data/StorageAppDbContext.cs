62c645f18758ada0b082a5b32007f64b314b2029
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-30 09:51:33 +1200

    create in memory db

---
+++ WiredBrainCoffee.StorageApp/Data/StorageAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Data
+{
+    public class StorageAppDbContext : DbContext
+    {
+        //properties
+        public DbSet<Employee> Employees => Set<Employee>();
+        public DbSet<Organization> Organizations => Set<Organization>();
+
+        //methods
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("StorageAppDb");
+        }
+    } 
+}
