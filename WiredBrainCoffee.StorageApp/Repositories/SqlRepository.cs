6474e1b21fff3748bd3cf8b39c94960d01a90193
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-30 10:03:54 +1200

    refactor SqlRepository class

--- WiredBrainCoffee.StorageApp/Repositories/SqlRepository.cs
+++ WiredBrainCoffee.StorageApp/Repositories/SqlRepository.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.EntityFrameworkCore;
 using WiredBrainCoffee.StorageApp.Entities;
 
 namespace WiredBrainCoffee.StorageApp.Repositories
 {
-    public class SqlRepository<T> where T : IEntity
+    public class SqlRepository<T> where T : class, IEntity
     {
+        private readonly DbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
 
-        private readonly List<T> _items = new();// shorthand for new List<T>()
+        public SqlRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<T>();
+        }
 
         //methods
 
         public T GetById(int id)
         {
-            return _items.Single(item => item.Id == id);
+            return _dbSet.Find(id);
         }
 
         public void Add(T item)
         {
-            item.Id = _items.Count + 1;
-            _items.Add(item);
+            _dbSet.Add(item);
         }
 
         public void Remove(T item)
         {
-            _items.Remove(item);
+            _dbSet.Remove(item);
         }
 
         public void Save()
         {
-            foreach (var item in _items)
-            {
-                Console.WriteLine(item);
-            }
+            _dbContext.SaveChanges();
         }
     }
 
