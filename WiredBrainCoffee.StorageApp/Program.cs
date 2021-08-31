2e653122601e984bf83ae8bdfec20236bbc13edf
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-31 15:51:33 +1200

    refactor AddManager as extension method

--- WiredBrainCoffee.StorageApp/Program.cs
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -66,7 +66,7 @@ namespace WiredBrainCoffee.StorageApp
                 new Employee { FirstName = "Anna" },
                 new Employee { FirstName = "Thomas" }
             };
-            AddBatch<Employee>(employeeRepository, employees);
+            employeeRepository.AddBatch(employees);
         }
 
         private static void AddOrganizations(IRepository<Organization> organizationRepository)
@@ -76,16 +76,7 @@ namespace WiredBrainCoffee.StorageApp
                 new Organization { Name = "Pluralsight" },
                 new Organization { Name = "Globomantics" }
             };
-            AddBatch(organizationRepository, organizations);
-        }
-
-        private static void AddBatch<T>(IWriteRepository<T> repository, T[] items)
-        {
-            foreach (var item in items)
-            {
-                repository.Add(item);
-            }
-                repository.Save();
+            organizationRepository.AddBatch(organizations);
         }
     }
 }
---
+++ WiredBrainCoffee.StorageApp/Repositories/RepositoryExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace WiredBrainCoffee.StorageApp.Repositories
+{
+    public static class RepositoryExtensions
+    {
+        public static void AddBatch<T>(this IWriteRepository<T> repository, T[] items)// this keyword makes method an extension of the IWriteRepository
+        {
+            foreach (var item in items)
+            {
+                repository.Add(item);
+            }
+            repository.Save();
+        }
+    }
+}
